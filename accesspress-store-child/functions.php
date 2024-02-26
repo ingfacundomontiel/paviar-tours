0eac0e3b536a7cc2c6b182ec93e1a375365a38a5
facundomontiel <facundomontiel22@gmail.com>
2024-02-26 11:38:21 -0300

    Paviar Tours Theme: Add Image Gallery Template

--- accesspress-store-child/functions.php
+++ accesspress-store-child/functions.php
@@ -1,30 +1,51 @@
 <?php
 // Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) {
+	exit;
+}
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'font-awesome','animate','slick','ticker' ) );
-    }
+if (!function_exists('chld_thm_cfg_parent_css')) :
+	function chld_thm_cfg_parent_css()
+	{
+		wp_enqueue_style('chld_thm_cfg_parent', trailingslashit(get_template_directory_uri()) . 'style.css', array('font-awesome', 'animate', 'slick', 'ticker'));
+	}
 endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+add_action('wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10);
 
 // END ENQUEUE PARENT ACTION
 
-//Cambiar Texto de añadir al carrito
-add_filter( 'add_to_cart_text', 'texto_personalizado_boton_anadir_carrito_archivo' ); // versiones menores a 2.1
-add_filter( 'woocommerce_product_add_to_cart_text', 'texto_personalizado_boton_anadir_carrito_archivo' ); // 2.1 o superior
-function texto_personalizado_boton_anadir_carrito_archivo() {
-return __( 'Ver Tour', 'woocommerce' );
+// Cambiar Texto de añadir al carrito
+add_filter('add_to_cart_text', 'texto_personalizado_boton_anadir_carrito_archivo'); // versiones menores a 2.1
+add_filter('woocommerce_product_add_to_cart_text', 'texto_personalizado_boton_anadir_carrito_archivo'); // 2.1 o superior
+
+function texto_personalizado_boton_anadir_carrito_archivo()
+{
+	return __('Ver Tour', 'woocommerce');
 }
 
-//Eliminar campo Web de Comentarios en Blog
+// Eliminar campo Web de Comentarios en Blog
 
-function dcms_disable_url_comment($fields) { 
-    unset($fields['url']);
-    return $fields;
+function dcms_disable_url_comment($fields)
+{
+	unset($fields['url']);
+	return $fields;
 }
-add_filter('comment_form_default_fields','dcms_disable_url_comment');
+add_filter('comment_form_default_fields', 'dcms_disable_url_comment');
+
+
+// Agregar Scripts y Estilos para la página de Galería de Viajeros
+
+if (!function_exists('chld_thm_cfg_gallery_page')) :
+	function chld_thm_cfg_gallery_page()
+	{
+		if (is_page_template('page-image-gallery.php')) {
+			wp_enqueue_style('image-gallery-style', get_template_directory_uri() . '-child/css/image-gallery.css', array(), null);
+			wp_enqueue_script('image-gallery-scripts', get_template_directory_uri() . '-child/js/image-gallery.js', array(), null, true);
+		}
+	}
+endif;
+
+add_action('wp_enqueue_scripts', 'chld_thm_cfg_gallery_page', 10);
---
+++ accesspress-store-child/page-image-gallery.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * Template Name: Galería de Imágenes
+ * 
+ * The template for displaying a Page with an Image Gallery.
+ *
+ * @package AccessPress Store
+ */
+get_header();
+
+global $post;
+$single_page_layout = esc_attr(get_post_meta($post->ID, 'accesspress_store_sidebar_layout', true));
+if (empty($single_page_layout)) {
+    $single_page_layout = esc_attr(get_theme_mod('single_page_layout', 'right-sidebar'));
+}
+?>
+<div class="inner">
+    <main id="main" class="site-main clearfix <?php echo esc_attr($single_page_layout); ?>">
+        <div class="entry-content">
+
+            <div class="content-inner clearfix">
+                <h2 class="image-gallery-page-title"><?php the_title(); ?></h2>
+                <div class="content-page">
+                    <?php
+                    while (have_posts()) :
+                        the_post();
+                        the_content();
+                    endwhile; // end of the loop.
+                    ?>
+                </div>
+
+                <div class="gallery-container">
+                    <div class="gallery-item" data-index="1">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/bicho-godano.jpg' ?>">
+                        <p class="traveler-image-caption">
+                            Bicho Godano
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="2">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/bicho-godano-2.jpg' ?>">
+                        <p class="traveler-image-caption">
+                            Bicho Godano
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="3">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/daniel-arcucci.jpeg' ?>">
+                        <p class="traveler-image-caption">
+                            Daniel Arcucci
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="4">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/dany-moreno.jpeg' ?>">
+                        <p class="traveler-image-caption">
+                            Dany Moreno
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="5">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/fantino.jpeg' ?>">
+                        <p class="traveler-image-caption">
+                            Alejandro Fantino
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="6">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/lito-pintos.jpeg' ?>">
+                        <p class="traveler-image-caption">
+                            Lito Pintos
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="7">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/mariana-fabiani.jpg' ?>">
+                        <p class="traveler-image-caption">
+                            Mariana Fabiani
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="8">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/maxi-pullaro.jpg' ?>">
+                        <p class="traveler-image-caption">
+                            Maxi Pullaro
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="9">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/nino-tuburcio.jpg' ?>">
+                        <p class="traveler-image-caption">
+                            Nino Tuburcio
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="10">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/unknown.jpg' ?>">
+                        <p class="traveler-image-caption">
+                            Unknown
+                        </p>
+                    </div>
+                    <div class="gallery-item" data-index="10">
+                        <img src="<?php echo get_template_directory_uri() . '-child/img/unknown-2.jpg' ?>">
+                        <p class="traveler-image-caption">
+                            Unknown
+                        </p>
+                    </div>
+
+                </div>
+            </div> <!-- .content-inner -->
+        </div> <!-- .entry-content -->
+    </main>
+</div>
+
+<?php get_footer();
